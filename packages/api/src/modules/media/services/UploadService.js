499eae89257eac4340c889deedad2affd3ab24fe
Cristian Incarnato <cristian.cdi@gmail.com>
2020-07-12 22:07:32 -0300

    Add hash, and dir path

--- packages/api/src/modules/media/services/UploadService.js
+++ packages/api/src/modules/media/services/UploadService.js
@@ -2,15 +2,16 @@ import path from "path";
 import fs from "fs";
 import file from '../models/FileModel'
 import storeFile from './helpers/storeFile'
+import randomString from './helpers/randomString'
 
-const baseUrl = function(){
+const baseUrl = function () {
     let url = process.env.APP_API_URL
 
-    if(!/^http:\/\//.test(url) ){
+    if (!/^http:\/\//.test(url)) {
         url = "http://" + baseUrl
     }
 
-    if(!/\/$/.test(url)){
+    if (!/\/$/.test(url)) {
         url += "/"
     }
 
@@ -23,25 +24,29 @@ const fileUpload = function (user, inputFile) {
 
         const {filename, mimetype, encoding, createReadStream} = await inputFile;
 
-        console.log('mimetype',mimetype)
-        console.log('encoding',encoding)
+        console.log('mimetype', mimetype)
+        console.log('encoding', encoding)
 
         let type = mimetype.split("/")[0]
 
         const parseFileName = path.parse(filename);
         const extension = parseFileName.ext
-        const relativePath = path.join("media", "files", filename)
+        const name = parseFileName.name
+        const hash = '-' + randomString(6)
+        const finalFileName = name + hash + extension
+        const year = new Date().getFullYear().toString()
+        const relativePath = path.join("media", "files", user.username, year, finalFileName)
         const absolutePath = path.resolve(relativePath);
 
         //Store
         let storeResult = await storeFile(createReadStream(), relativePath)
 
-        let url = baseUrl() +  relativePath
+        let url = baseUrl() + relativePath
 
         if (storeResult && storeResult.finish) {
 
             file.create({
-                filename: filename,
+                filename: finalFileName,
                 mimetype: mimetype,
                 encoding: encoding,
                 type: type,
@@ -54,7 +59,7 @@ const fileUpload = function (user, inputFile) {
             }, function (err, doc) {
                 if (err) return rejects(err);
                 // saved!
-                doc.populate('createdBy').execPopulate(() => ( resolve(doc) ) )
+                doc.populate('createdBy').execPopulate(() => (resolve(doc)))
             });
 
         } else {
