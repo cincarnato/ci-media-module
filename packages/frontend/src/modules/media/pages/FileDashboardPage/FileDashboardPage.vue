01559ec7e76cd9cd7e22cf6812d11ddf67696b2c
Cristian Incarnato <cristian.cdi@gmail.com>
2020-07-18 11:33:26 -0300

    Some change on dashboard

--- packages/frontend/src/modules/media/pages/FileDashboardPage/FileDashboardPage.vue
+++ packages/frontend/src/modules/media/pages/FileDashboardPage/FileDashboardPage.vue
@@ -1,45 +1,38 @@
 <template>
     <v-container>
-        <v-card>
-            <v-card-title>
-                {{$t('media.file.dashboardTitle')}}
-            </v-card-title>
-
-            <v-card-text>
-                <v-row>
-                    <v-col cols="12" sm="4" offset-sm="2">
-                        <v-card color="blue" dark>
-                            <v-row>
-                                <v-col cols="3" class="text-center display-1">
-                                    <v-icon size="90">loupe</v-icon>
-                                </v-col>
-                                <v-col cols="9">
-                                    <h6 class="text-h6">COUNT</h6>
-                                    <h1 class=" text-center text-h3 font-weight-bold">
-                                        {{fileGlobalMetrics.count}}
-                                    </h1>
-                                </v-col>
-                            </v-row>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="12" sm="4">
-                        <v-card color="red" dark>
-                            <v-row>
-                                <v-col cols="3" class="text-center display-1"><v-icon size="90">fitness_center</v-icon></v-col>
-                                <v-col cols="9">
-                                    <h6 class="text-h6">WEIGHT</h6>
-                                    <h1 class=" text-center text-h3 font-weight-bold">
-                                        {{redeableBytes(fileGlobalMetrics.weight)}}
-                                    </h1>
-                                </v-col>
-                            </v-row>
-                        </v-card>
-                    </v-col>
-                </v-row>
-
-
-            </v-card-text>
-        </v-card>
+        <h5 class="text-h5">    {{$t('media.file.dashboardTitle')}}</h5>
+        <v-row>
+            <v-col cols="12" sm="4" offset-sm="2">
+                <v-card color="blue" dark>
+                    <v-row>
+                        <v-col cols="3" class="text-center">
+                            <v-icon size="80">loupe</v-icon>
+                        </v-col>
+                        <v-col cols="9">
+                            <h6 class="text-h6">COUNT</h6>
+                            <h1 class=" text-h3 font-weight-bold">
+                                {{fileGlobalMetrics.count}}
+                            </h1>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <v-card color="red" dark>
+                    <v-row>
+                        <v-col cols="3" class="text-center">
+                            <v-icon size="80">fitness_center</v-icon>
+                        </v-col>
+                        <v-col cols="9">
+                            <h6 class="text-h6">WEIGHT</h6>
+                            <h1 class=" text-h3 font-weight-bold">
+                                {{redeableBytes(fileGlobalMetrics.weight)}}
+                            </h1>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-col>
+        </v-row>
 
     </v-container>
 </template>
